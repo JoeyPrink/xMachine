6e0a39d562898849a4d844f505d1480eed16d5c9
Johanna Pirker <joeyprink@gmail.com>
2018-12-15 14:07:40 +0100

    ready for bug search

--- XmasMachine/Assets/AlertCollision.cs
+++ XmasMachine/Assets/AlertCollision.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AlertCollision : MonoBehaviour
 {
@@ -40,9 +42,16 @@ public class AlertCollision : MonoBehaviour
             int currentLives = coreLoop.getPlayerLives() -1;
 
             coreLoop.PlayerLives = currentLives;
+
+            if(currentLives == 0 ) {
+                // TODO Game Over 
+
+                restartLevel();
+
+            } 
             //Debug.Log("current lives... " + currentLives);
 
-            //Destroy(tmpGO);
+          
 
 
             for (int i = 0; i < stringManager.DisplayTexts.Count; i++){
@@ -50,17 +59,22 @@ public class AlertCollision : MonoBehaviour
                 Debug.Log("..." + i);
 
                 //Debug.Log(stringManager.DisplayTexts[i].ToString());
-
                 //Debug.Log("..." + i + ": "+ stringManager.DisplayTexts[i].text);
 
                 if (stringManager.DisplayTexts[i] != null)
                 {
                     Debug.Log("display :" + i + "::"+ stringManager.DisplayTexts[i].text);
+                    if(stringManager.DisplayTexts[i].Equals(currentTextMesh)) {
+                        stringManager.DisplayTexts.Remove(currentTextMesh);
+                    }
+                        
                 }
                /* if (stringManager.DisplayTexts[i].text == tmpString) {
-                    Debug.Log("yeaaaaaaah");
+
                 }*/
             }
+
+            Destroy(tmpGO);
 
 
 
@@ -72,4 +86,12 @@ public class AlertCollision : MonoBehaviour
 
     }
 
+    private void restartLevel()
+    {
+        SceneManager.LoadScene("0-startScreen");
+        coreLoop.PlayerLives = 3;
+        stringManager.DisplayTexts.Clear();
+        
+
+    }
 }
